5052da3094dbadc989c65093308da7fcf79263f1
sanjeev <sanjeevkomma@gmail.com>
2025-04-27 22:54:10 +0530

    in

--- React/projects/todo-app/src/index.js
+++ React/projects/todo-app/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App.js';
+import App123 from './App.js';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render( <App /> , document.getElementById('root123') );
+ReactDOM.render( <App123 /> , document.getElementById('root123') );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
