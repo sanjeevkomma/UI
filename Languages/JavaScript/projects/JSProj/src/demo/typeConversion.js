9479de671647c1b2fb1b3bb21f749ab2d960b5d7
sanjeev <sanjeevkomma@gmail.com>
2025-04-28 22:56:26 +0530

    in

---
+++ Languages/JavaScript/projects/JSProj/src/demo/typeConversion.js
@@ -0,0 +1,54 @@
+let str = String(6);
+console.log(str, typeof str);
+
+
+let num = Number("123");
+console.log(num, typeof num);
+
+
+let x;
+console.log(x, typeof x);
+
+x = 8;
+console.log(x, typeof x); // 8 number
+
+x = x + "";
+console.log(x, typeof x); // 8 string
+
+x = x - 2;
+console.log(x, typeof x); // 6 number
+
+x = !x;
+console.log(x, typeof x); // false boolean
+
+console.log(Boolean(7)); // true --- Truthy value
+console.log(Boolean(-7)); // true
+console.log(Boolean(0)); // false
+console.log(Boolean(null)); // false -- Falsy value
+console.log(Boolean(undefined)); // false -- Falsy value
+console.log(Boolean("sanjeev")); // true
+console.log(Boolean(!"sanjeev")); // false
+
+let y = Number("sanjeev 123");
+console.log(y); // NaN
+
+let z = parseInt("sanjeev 123");
+console.log(z); // NaN
+
+let b = parseInt("123 sanjeev");
+console.log(b); // 123
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
