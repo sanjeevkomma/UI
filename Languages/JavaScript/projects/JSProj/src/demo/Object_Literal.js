d79d36926ca1592266b011f949141f28be821747
Sanjeev Komma <sanjeevkomma@gmail.com>
2025-04-29 16:25:03 +0530

    Update Object_Literal.js

--- Languages/JavaScript/projects/JSProj/src/demo/Object_Literal.js
+++ Languages/JavaScript/projects/JSProj/src/demo/Object_Literal.js
@@ -6,8 +6,7 @@ let input = 'name';
 let alien = {
   name : "Sanjeev",
   tech : "JS",
-  'work exp' : 4,
-  laptop : {
+    laptop : {
     cpu : 'i7',
     ram : 4,
     brand : 'dell'
@@ -18,13 +17,29 @@ console.log(alien);
 console.log("name : " + alien.name);
 console.log("tech : " + alien.tech);
 console.log("name : " + alien["name"]);
-console.log("work exp : " + alien["work exp"]);
+// console.log("work exp : " + alien["work exp"]);
 console.log("name : " + alien[input]);
 console.log(alien.laptop.brand); // dell
 console.log(alien.laptop.brand.length); // 4
 console.log(alien.laptop.brand1?.length); // undefined
 
-delete alien.laptop; // delete laptop from alien
-console.log(alien); // { name: 'Sanjeev', tech: 'JS', 'work exp': 4 }
+// delete alien.laptop; // delete laptop from alien
+// console.log(alien); // { name: 'Sanjeev', tech: 'JS' }
 
-//-----------
+//------For In Loop-----
+for(let key in alien){
+  console.log(key);
+}
+for(let key in alien){
+  console.log(key, alien[key]);
+}
+for(let key in alien.laptop){
+  console.log(key, alien.laptop[key]);
+}
+
+//----for...of loop : Used for iterable collection
+let fruits = ["apple", "banana", "cherry"];
+
+for (let fruit of fruits) {
+  console.log(fruit);
+}
