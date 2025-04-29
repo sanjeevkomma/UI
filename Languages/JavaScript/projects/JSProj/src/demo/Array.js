b5e84e557a8e41141a2ef934e011f8b8757e9733
Sanjeev Komma <sanjeevkomma@gmail.com>
2025-04-29 19:50:57 +0530

    Create Array.js

---
+++ Languages/JavaScript/projects/JSProj/src/demo/Array.js
@@ -0,0 +1,14 @@
+let arr = [1, 2, 3, 4, 5];
+console.log(arr); // [ 1, 2, 3, 4, 5 ]
+console.log(arr.length); // 5
+console.log(arr[0]); // 1 -- index based
+console.log(arr[7]); // undefined
+
+let values = new Array();
+console.log(values); // []
+
+let values2 = [];
+console.log(values2); // []
+values2.push(12);
+console.log(values2);
+
