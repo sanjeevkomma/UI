425ddaaee01b4f4924c8c23acbfac18ecd164555
Sanjeev Komma <sanjeevkomma@gmail.com>
2025-04-29 13:26:37 +0530

    Create Template_Literal.js

---
+++ Languages/JavaScript/projects/JSProj/src/demo/Template_Literal.js
@@ -0,0 +1,7 @@
+let num1 = 7;
+let num2 = 4;
+let result = num1 + num2;
+console.log(`The addition of ${num1} and ${num2} is ` + `${result}`); // The addition of 7 and 4 is 11
+console.log(`My name is 
+Sanjeev Komma`); // My name is
+                 // Sanjeev Komma
