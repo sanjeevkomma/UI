6b479dfc47f504c3fc78bab909771245009289aa
Sanjeev Komma <sanjeevkomma@gmail.com>
2025-04-29 10:31:42 +0530

    Create isEven.js

---
+++ Languages/JavaScript/projects/JSProj/src/demo/isEven.js
@@ -0,0 +1,11 @@
+function isEven(num) {
+  if (num % 2 == 0) {
+    console.log(num + " is Even");
+  } else {
+    console.log(num + " is Odd");
+  }
+}
+
+// Example usage
+isEven(4);  // Output: 4 is Even
+isEven(7);  // Output: 7 is Odd
