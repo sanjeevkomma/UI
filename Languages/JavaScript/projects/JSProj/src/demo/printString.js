b707b2cd63d9a22a13a22ec4c62abb6fd9f8c631
Sanjeev Komma <sanjeevkomma@gmail.com>
2025-04-29 11:51:29 +0530

    Update printString.js

--- Languages/JavaScript/projects/JSProj/src/demo/printString.js
+++ Languages/JavaScript/projects/JSProj/src/demo/printString.js
@@ -1,16 +1,16 @@
 let str = "Hello world";
-console.log(str + " 123");
+console.log(str + " 123"); // Hello world 123
 
 const pi = 3.14;
 
 let userName = "sanjeev";
 
-console.log("userName : " + userName);
+console.log("userName : " + userName); // userName : sanjeev
 
 let num1 = 1n;
-console.log(num1 * 3n);
-console.log(typeof num1);
-console.log(Number.MAX_VALUE);
+console.log(num1 * 3n); // 3n
+console.log(typeof num1); // bigint
+console.log(Number.MAX_VALUE); // 1.7976931348623157e+308
 
 
 
@@ -19,13 +19,13 @@ let lastName = "komma \"data\"";
 
 let user = firstName + " " + lastName;
 
-console.log("user : " + user);
+console.log("user : " + user); // user : sanjeev komma "data"
 
 
 let bool = 5 > 6;
-console.log("bool : " + bool);
-console.log(typeof bool);
+console.log("bool : " + bool); // bool : false
+console.log(typeof bool); // boolean
 
 let num22 = 5;
 
-console.log( typeof ( 5 / "sanj"));
+console.log( typeof ( num22 / "sanj")); // number
