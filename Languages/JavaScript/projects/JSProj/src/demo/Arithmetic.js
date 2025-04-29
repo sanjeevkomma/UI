259f1b655612c3d440b7e8339318b79ac22f8b85
Sanjeev Komma <sanjeevkomma@gmail.com>
2025-04-29 11:40:48 +0530

    Create Arithmetic.js

---
+++ Languages/JavaScript/projects/JSProj/src/demo/Arithmetic.js
@@ -0,0 +1,29 @@
+let num1 = 4;
+let num2 = 2;
+console.log(num1 + num2); // prints 6
+
+let num3 = true;
+let num4 = true;
+
+let result = num3 + num4;
+
+console.log("result : " + result); // prints 2 : true = 1 ( Truthy value ), false = 0 (Falsy value)
+
+
+let num = 4;
+num = num + 2;
+num += 3;
+num ++; // post increment
+num = num - 5;
+++num; // pre increment
+console.log("num : " + num);
+
+let x = num ++;
+console.log(x,num);
+
+let y = ++ num;
+console.log(y,num);
+
+let a = 4;
+let result2 = Math.pow(4,3);
+console.log("result2 : " + result2);
