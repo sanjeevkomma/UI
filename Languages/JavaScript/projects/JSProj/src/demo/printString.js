a2bf294dfe069f88bc110b84a6e20e9d00d23618
Sanjeev Komma <sanjeevkomma@gmail.com>
2025-04-29 10:31:16 +0530

    Update printString.js

--- Languages/JavaScript/projects/JSProj/src/demo/printString.js
+++ Languages/JavaScript/projects/JSProj/src/demo/printString.js
@@ -1,2 +1,31 @@
-let str = "Hello World";
-console.log(str);
+let str = "Hello world";
+console.log(str + " 123");
+
+const pi = 3.14;
+
+let userName = "sanjeev";
+
+console.log("userName : " + userName);
+
+let num1 = 1n;
+console.log(num1 * 3n);
+console.log(typeof num1);
+console.log(Number.MAX_VALUE);
+
+
+
+let firstName = "sanjeev";
+let lastName = "komma \"data\"";
+
+let user = firstName + " " + lastName;
+
+console.log("user : " + user);
+
+
+let bool = 5 > 6;
+console.log("bool : " + bool);
+console.log(typeof bool);
+
+let num22 = 5;
+
+console.log( typeof ( 5 / "sanj"));
