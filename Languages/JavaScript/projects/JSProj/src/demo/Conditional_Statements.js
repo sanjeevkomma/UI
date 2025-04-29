0e546fc0076c20eb8c5d48de61d72e4d19ec5cfa
Sanjeev Komma <sanjeevkomma@gmail.com>
2025-04-29 12:44:07 +0530

    Create Conditional_Statements.js

---
+++ Languages/JavaScript/projects/JSProj/src/demo/Conditional_Statements.js
@@ -0,0 +1,18 @@
+let num1 = 6;
+let num2 = 4;
+let num3 = 7;
+if(num1 > num2 && num1 > num3) {
+    console.log("num1 is greater");
+    console.log("yee...");
+}
+else if ( num2 > num3){
+  console.log("num2 is greater");
+}
+else
+     console.log("num3 is greater");
+
+console.log("Bye ...");
+
+/* output : num3 is greater
+Bye ...
+*/
