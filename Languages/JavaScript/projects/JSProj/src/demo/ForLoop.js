b98cb333c4d0c9c0edc0599bc3b8dbe660684cba
Sanjeev Komma <sanjeevkomma@gmail.com>
2025-04-29 14:40:09 +0530

    Create ForLoop.js

---
+++ Languages/JavaScript/projects/JSProj/src/demo/ForLoop.js
@@ -0,0 +1,16 @@
+for (let i = 1; i <= 2; i++) {
+  console.log("Hi " + i);
+  for (let j = 1; j <= 2; j++) {
+    console.log("Hello",j);
+  }
+}
+
+// output :
+/*
+Hi 1
+Hello 1
+Hello 2
+Hi 2
+Hello 1
+Hello 2
+*/
