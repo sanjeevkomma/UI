f0a7b22fa64d392a4e778ce47ac156c7bb3c2075
Sanjeev Komma <sanjeevkomma@gmail.com>
2025-04-29 11:47:34 +0530

    Update Arithmetic_Operators.js

--- Languages/JavaScript/projects/JSProj/src/demo/Arithmetic_Operators.js
+++ Languages/JavaScript/projects/JSProj/src/demo/Arithmetic_Operators.js
@@ -16,14 +16,14 @@ num += 3;
 num ++; // post increment
 num = num - 5;
 ++num; // pre increment
-console.log("num : " + num);
+console.log("num : " + num); // prints 6
 
 let x = num ++;
-console.log(x,num);
+console.log(x,num); // prints 6 7
 
 let y = ++ num;
-console.log(y,num);
+console.log(y,num); // prints 8 8
 
 let a = 4;
 let result2 = Math.pow(4,3);
-console.log("result2 : " + result2);
+console.log("result2 : " + result2); // prints 64
