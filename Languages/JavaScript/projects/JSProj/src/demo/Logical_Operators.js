9269f7d9b4e1cd81cd5806bbeda9d9e9d045476c
Sanjeev Komma <sanjeevkomma@gmail.com>
2025-04-29 12:29:27 +0530

    Create Logical_Operators.js

---
+++ Languages/JavaScript/projects/JSProj/src/demo/Logical_Operators.js
@@ -0,0 +1,12 @@
+let x = 7, y = 8, z = 9;
+let result = x < y && x < z;
+console.log(result); // true
+
+
+let a = 4, b = 18, c = 6;
+let result1 = a < b || a < c;
+console.log(result1); // true
+
+let d = 12, e = 34, f = 343;
+let result3 = d < e || d < f;
+console.log(!result3); // false
