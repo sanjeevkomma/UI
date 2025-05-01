73297e8e9acd179044f99d29904db89a39be5ff9
sanjeev <sanjeevkomma@gmail.com>
2025-05-01 20:09:44 +0530

    in

--- Languages/JavaScript/projects/JSProj/src/demo/Set.js
+++ Languages/JavaScript/projects/JSProj/src/demo/Set.js
@@ -1,3 +1,4 @@
+// Set is collection of unique values and maintains insertion order
 let nums = new Set("bookkeeper");
 console.log(nums); // Set(6) { 'b', 'o', 'k', 'e', 'p', 'r' }
 
@@ -11,3 +12,19 @@ nums2.add("sanjeev");
 nums2.add(true);
 nums2.add("sanav");
 console.log(nums2); // Set(7) { 3, 4, 5, 33, 'sanjeev', true, 'sanav' }
+
+nums2.delete(5); // delete 5 from set
+
+nums2.forEach(value => {
+  console.log(value);
+});
+/* output :
+3
+4
+33
+'sanjeev'
+true
+'sanav'
+*/
+
+console.log(nums2.has(3)); // true
