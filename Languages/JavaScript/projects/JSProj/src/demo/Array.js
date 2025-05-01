7d0dd714d7fabf50e2546b1e91f87f242e889569
sanjeev <sanjeevkomma@gmail.com>
2025-05-01 13:40:08 +0530

    in

--- Languages/JavaScript/projects/JSProj/src/demo/Array.js
+++ Languages/JavaScript/projects/JSProj/src/demo/Array.js
@@ -27,12 +27,17 @@ console.log(data[5]());// Hello World SS
 
 
 let data2 = [5,7,8,9];
-console.log(data2.push(2)); // 5 --> i) add an element to ending & returns length of array after pushing an element
+console.log(data2.push(2)); // 5 (length of an array after pushing) --> i) add an element to ending & returns length of array after pushing an element
 console.log(data2); // [ 5, 7, 8, 9, 2 ]
-console.log(data2.pop()); // 2
+console.log(data2.pop()); // 2 -- -- removes an element from the ending
 console.log(data2); // [ 5, 7, 8, 9 ]
 console.log(data2.shift()); // 5 -- removes an element from the beginning
 console.log(data2); // [ 7, 8, 9 ]
 console.log(data2.unshift(2)); // 4 -- add an element to beginning and returns length of an array after adding an element
 console.log(data2); // [ 2, 7, 8, 9 ]
-console.log(data2.splice(1,2)); // [ 7, 8 ]
+console.log(data2.splice(1,2)); // removes [ 7, 8 ] -- count 2,  starting from index 1
+console.log(data2); // [ 2, 9 ]
+console.log(data2.splice(0,2,14,15)); // replaces [ 2, 9 ] with [ 14, 15 ] -- count 2, starting from index 0
+console.log(data2); // [ 14, 15 ]
+console.log(data2.push(45,46,47)); // 5 (length of an array after pushing)
+console.log(data2); // [ 14, 15, 45, 46, 47 ]
