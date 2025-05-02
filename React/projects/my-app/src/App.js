a7ca59b701642b7f61316b3e775b6147179c2c3f
sanjeev <sanjeevkomma@gmail.com>
2025-05-02 20:25:00 +0530

    in

--- React/projects/my-app/src/App.js
+++ React/projects/my-app/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Apple from './Apple';
 import Sample from './example/Sample';
+import './sample.css';
 
 function App() {
   return (
     <div className="App">
         <Apple />
         <Sample />
+        <h1>This is sample CSS file</h1>
     </div>
   );
 }
