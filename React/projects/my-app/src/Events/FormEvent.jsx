56fcb1d168e98f7ca03fe0d5d68e9445460584f3
sanjeev <sanjeevkomma@gmail.com>
2025-05-07 18:17:06 +0530

    in

--- React/projects/my-app/src/Events/FormEvent.jsx
+++ React/projects/my-app/src/Events/FormEvent.jsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
+
 const FormEvent = () => {
+
+    const [userName, setUserName] = useState("");
+    const getUserName = (event) => {
+        setUserName(event.target.value);
+    }
+
+    const [newUserDetails, setNewUserDetails] = useState();
+
+    const userDetails = () => {
+           setNewUserDetails(userName);
+    }
+
     return (
         <section className="formSection">
-        <div>
-            <input type="text" placeholder="Enter your name"/>
-            <br/>
-            <button>Submit</button>
-        </div>
+            <h2> onChange {userName}</h2>
+             <h2> onClick {newUserDetails}</h2>
+            <div className="inputDiv">
+                <input type="text" placeholder="Enter your name"
+                       onChange={getUserName}/>
+
+                <br/>
+                <button className="Submitbt" onClick={userDetails}>Submit</button>
+            </div>
         </section>
     );
 }
+
 export default FormEvent;
